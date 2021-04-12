0cb3f8a898f2c13e02d36605e9867875b13788c5
Swarna Saraf <swsaraf@adobe.com>
2021-04-12 10:18:59 -0700

    Implement Personalization Public Interface (#5)
    * Offer Public Classes + Unit tests
    
    * feedback fixes
    
    * updated swiftlint yml
    
    * Lint test fixes + update config yml
    
    * more lint updates
    
    * Lowered deployment target for tests + ci config fixes
    
    * Implement Personalization Public APIs
    
    * Update circle ci config.yml
    
    * Updated log messages as per feedback

--- Tests/AEPEdgePersonalizationTests/UnitTests/DecisionScopeTests.swift
+++ Tests/AEPEdgePersonalizationTests/UnitTests/DecisionScopeTests.swift
@@ -10,7 +10,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import AEPEdgePersonalization
+@testable import AEPEdgePersonalization
 import XCTest
 
 class DecisionScopeTests: XCTestCase {
@@ -26,24 +26,16 @@ class DecisionScopeTests: XCTestCase {
     }
 
     func testConvenienceInit_withDefaultItemCount() {
-        guard let decisionScope = DecisionScope(activityId: "xcore:offer-activity:1111111111111111",
-                                                placementId: "xcore:offer-placement:1111111111111111")
-        else {
-            XCTFail("Decision Scope should not be nil.")
-            return
-        }
+        let decisionScope = DecisionScope(activityId: "xcore:offer-activity:1111111111111111",
+                                          placementId: "xcore:offer-placement:1111111111111111")
 
         XCTAssertEqual("eyJhY3Rpdml0eUlkIjoieGNvcmU6b2ZmZXItYWN0aXZpdHk6MTExMTExMTExMTExMTExMSIsInBsYWNlbWVudElkIjoieGNvcmU6b2ZmZXItcGxhY2VtZW50OjExMTExMTExMTExMTExMTEifQ==", decisionScope.name)
     }
 
     func testConvenienceInit_withItemCount() {
-        guard let decisionScope = DecisionScope(activityId: "xcore:offer-activity:1111111111111111",
-                                                placementId: "xcore:offer-placement:1111111111111111",
-                                                itemCount: 10)
-        else {
-            XCTFail("Decision Scope should not be nil.")
-            return
-        }
+        let decisionScope = DecisionScope(activityId: "xcore:offer-activity:1111111111111111",
+                                          placementId: "xcore:offer-placement:1111111111111111",
+                                          itemCount: 10)
 
         XCTAssertEqual("eyJhY3Rpdml0eUlkIjoieGNvcmU6b2ZmZXItYWN0aXZpdHk6MTExMTExMTExMTExMTExMSIsInBsYWNlbWVudElkIjoieGNvcmU6b2ZmZXItcGxhY2VtZW50OjExMTExMTExMTExMTExMTEiLCJpdGVtQ291bnQiOjEwfQ==", decisionScope.name)
     }
@@ -51,19 +43,61 @@ class DecisionScopeTests: XCTestCase {
     func testConvenienceInit_withEmptyActivityId() {
         let decisionScope = DecisionScope(activityId: "",
                                           placementId: "xcore:offer-placement:1111111111111111")
-        XCTAssertNil(decisionScope)
+        XCTAssertEqual("eyJhY3Rpdml0eUlkIjoiIiwicGxhY2VtZW50SWQiOiJ4Y29yZTpvZmZlci1wbGFjZW1lbnQ6MTExMTExMTExMTExMTExMSJ9", decisionScope.name)
     }
 
     func testConvenienceInit_withEmptyPlacementId() {
         let decisionScope = DecisionScope(activityId: "xcore:offer-activity:1111111111111111",
                                           placementId: "")
-        XCTAssertNil(decisionScope)
+        XCTAssertEqual("eyJhY3Rpdml0eUlkIjoieGNvcmU6b2ZmZXItYWN0aXZpdHk6MTExMTExMTExMTExMTExMSIsInBsYWNlbWVudElkIjoiIn0=", decisionScope.name)
     }
 
     func testConvenienceInit_withZeroItemCount() {
         let decisionScope = DecisionScope(activityId: "xcore:offer-activity:1111111111111111",
                                           placementId: "xcore:offer-placement:1111111111111111",
                                           itemCount: 0)
-        XCTAssertNil(decisionScope)
+        XCTAssertEqual("eyJhY3Rpdml0eUlkIjoieGNvcmU6b2ZmZXItYWN0aXZpdHk6MTExMTExMTExMTExMTExMSIsInBsYWNlbWVudElkIjoieGNvcmU6b2ZmZXItcGxhY2VtZW50OjExMTExMTExMTExMTExMTEifQ==", decisionScope.name)
+    }
+
+    func testIsValid_scopeWithValidName() {
+        let decisionScope = DecisionScope(name: "myMbox")
+        XCTAssertTrue(decisionScope.isValid)
+    }
+
+    func testIsValid_scopeWithEmptyName() {
+        let decisionScope = DecisionScope(name: "")
+        XCTAssertFalse(decisionScope.isValid)
+    }
+
+    func testIsValid_scopeWithDefaultItemCount() {
+        let decisionScope = DecisionScope(activityId: "xcore:offer-activity:1111111111111111",
+                                          placementId: "xcore:offer-placement:1111111111111111")
+        XCTAssertTrue(decisionScope.isValid)
+    }
+
+    func testIsValid_scopeWithItemCount() {
+        let decisionScope = DecisionScope(activityId: "xcore:offer-activity:1111111111111111",
+                                          placementId: "xcore:offer-placement:1111111111111111",
+                                          itemCount: 10)
+        XCTAssertTrue(decisionScope.isValid)
+    }
+
+    func testIsValid_withEmptyActivityId() {
+        let decisionScope = DecisionScope(activityId: "",
+                                          placementId: "xcore:offer-placement:1111111111111111")
+        XCTAssertFalse(decisionScope.isValid)
+    }
+
+    func testIsValid_withEmptyPlacementId() {
+        let decisionScope = DecisionScope(activityId: "xcore:offer-activity:1111111111111111",
+                                          placementId: "")
+        XCTAssertFalse(decisionScope.isValid)
+    }
+
+    func testIsValid_withZeroItemCount() {
+        let decisionScope = DecisionScope(activityId: "xcore:offer-activity:1111111111111111",
+                                          placementId: "xcore:offer-placement:1111111111111111",
+                                          itemCount: 0)
+        XCTAssertTrue(decisionScope.isValid)
     }
 }
