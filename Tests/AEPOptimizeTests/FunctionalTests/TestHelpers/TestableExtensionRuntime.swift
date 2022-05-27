24e2aa45559d50faa3735e76db252e2ac5d85a84
Swarna Saraf <swsaraf@adobe.com>
2022-05-27 13:53:19 -0700

    Updated xcode version in circleci .yml to match Core.

--- Tests/AEPOptimizeTests/FunctionalTests/TestHelpers/TestableExtensionRuntime.swift
+++ Tests/AEPOptimizeTests/FunctionalTests/TestHelpers/TestableExtensionRuntime.swift
@@ -60,7 +60,7 @@ class TestableExtensionRuntime: ExtensionRuntime {
         }
         return mockedSharedStates["\(extensionName)"]
     }
-    
+
     func createXDMSharedState(data: [String: Any], event: Event?) {
         createdXdmSharedStates += [data]
     }
@@ -74,7 +74,7 @@ class TestableExtensionRuntime: ExtensionRuntime {
     func getXDMSharedState(extensionName: String, event: Event?, barrier: Bool = false) -> SharedStateResult? {
         getXDMSharedState(extensionName: extensionName, event: event, barrier: barrier, resolution: .any)
     }
-    
+
     func getXDMSharedState(extensionName: String, event: Event?, barrier: Bool, resolution: SharedStateResolution = .any) -> SharedStateResult? {
         // if there is an shared state setup for the specific (extension, event id) pair, return it. Otherwise, return the shared state that is setup for the extension.
         if let id = event?.id {
