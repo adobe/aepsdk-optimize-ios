cd643cf61f2b8a7c20d7bcc5f86b41ee2a96a1ef
Swarna Saraf <swsaraf@adobe.com>
2022-10-26 20:06:44 -0700

    Added Pods for tutorial starter and final apps

---
+++ docs/tutorials/OptimizeTutorialFinalApp/Pods/Target Support Files/AEPOptimize/AEPOptimize-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AEPOptimize : NSObject
+@end
+@implementation PodsDummy_AEPOptimize
+@end
---
+++ docs/tutorials/OptimizeTutorialStarterApp/Pods/Target Support Files/Pods-OptimizeTutorialStarterApp/Pods-OptimizeTutorialStarterApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OptimizeTutorialStarterApp : NSObject
+@end
+@implementation PodsDummy_Pods_OptimizeTutorialStarterApp
+@end
